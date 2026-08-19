282711f6645c7b2be4137e4bd8855c2d4a646610
giokhomaa <giokhomaa@gmail.com>
2026-08-19 21:14:44 +0400

    done lecture 9

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,17 +137,14 @@ MAILERS = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ],
-}
-
-REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt'
         '.authentication.JWTAuthentication',
         'rest_framework.authentication'
         '.SessionAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
 }
 
